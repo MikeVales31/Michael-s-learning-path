ae8520e67b7de7b3b4a182f38b53995c5e5530f6
Vales, Michael <Michael.Vales+atsolera@Solera.com>
2025-08-21 22:36:32 -0600

    bootcamp

---
+++ JavaScript/JS/Excersise/basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+let userInput = 3;
+let result;
+result = userInput + 18;
+result = result -1;
+result = result * 2;
+result = result / 4;
+
+alert(result); 
+alert(userInput);
---
+++ JavaScript/JS/Material/Section 2/basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,18 @@
+/*
+Variables 
+Data storage  (let)
+
+Constants
+you cannot change the value (const)
+
+
+You are allowed to skip semicolon at the end of the line
+*/
+
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calcuculationDescription = " (" + currentResult +  " + 10) * 3 / 2 - 1"
+
+outputResult(currentResult, calcuculationDescription);
